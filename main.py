9238d9e2c1b5b045559817d7a59bec61db69e9ca
Khang <khang86175xt@gmail.com>
2025-08-15 16:34:27 +0700

    Refactor game initialization to allow dynamic map sizes and improve bomb placement logic

--- main.py
+++ main.py
@@ -7,53 +7,53 @@ hướng dẫn chơi :
 '''
 # game_base hiện thị full map : bom là -1, số bom xung quanh là số khác
 # game_display hiện thị map đã chơi : 0 là chưa chơi, 1 là đã chơi
-game_base=[[0 for _ in range(12)] for _ in range(12)]
-game_display=[[0 for _ in range(12)] for _ in range(12)]
+n,m=map(int, input('Enter size of map (n row m col): ').split())
+game_base=[[0 for _ in range(m+2)] for _ in range(n+2)]
+game_display=[[0 for _ in range(m+2)] for _ in range(n+2)]
+num_bombs = 0
 
 # tạo bom ngẫu nhiên
-for i in range(1,11):
-    for j in range(1,11):
-        if(random.randint(0, 3) == 0):
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if( random.randint(0,3) == 0):
             game_base[i][j] = -1
-
+            num_bombs += 1
 # đếm số bom xung quanh mỗi ô
-for i in range(1,11):
-    for j in range(1,11):
+num_bombs = 0
+for i in range(1,n+1):
+    for j in range(1,m+1):
         if game_base[i][j] != -1:
             count = 0
-            if game_base[i][j] != -1:
-                for x in range(i-1, i+2):
-                    for y in range(j-1, j+2):
-                        if game_base[x][y] == -1:
-                            count += 1
+            for x in range(i-1, i+2):
+                for y in range(j-1, j+2):
+                    if game_base[x][y] == -1:
+                        count += 1
             game_base[i][j] = count
+        else:
+            num_bombs += 1
 
 # hiển thị bản đồ ban đầu
-print("Welcome to Minesweeper!")
-for i in range(12):
-    for j in range(12):
+print("Welcome to Minesweeper! ",num_bombs)
+for i in range(n):
+    for j in range(m):
         print('0 ',end='')
     print()
+
 # bắt đầu trò chơi
 step=0
 while True:
-    # đào hết thì thắng ( chắc vậy =] )
-    step += 1
-    if step == 74:
-        print("You win!")
-        break
 
     # nhập tọa độ muốn đào
-    n,m= map(int, input('Enter coordinates (row col): ').split())
-    game_display[n-1][m-1] = 1
-    if game_base[n-1][m-1] == -1:
+    x,y= map(int, input('Enter coordinates (row col): ').split())
+    game_display[x][y] = 1
+    if game_base[x][y] == -1:
         print("Game Over! point: ", step)
         break
 
     # hiển thị bản đồ sau mỗi bước
-    print("Current map:")
-    for i in range(12):
-        for j in range(12):
+    print("Current map (bombs ", num_bombs, "):")
+    for i in range(1,n+1):
+        for j in range(1,m+1):
             if(game_display[i][j] == 0):
                 print('0 ',end='')
             elif(game_base[i][j] == 0):
@@ -62,3 +62,9 @@ while True:
                 print(game_base[i][j], end='')
                 print(' ', end = '')
         print()
+    
+    # đào hết thì thắng ( chắc vậy =] )
+    step += 1
+    if step == (n)*(m) - num_bombs:
+        print("You win!")
+        break
