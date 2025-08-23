a43c4f2bfa26ebedb5fc4a687c16563d6c20e899
PPIG2204 <phuocnnn2006@gmail.com>
2025-08-23 17:43:51 +0700

    thêm phần tải thư viện khi chạy

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import sys
+import subprocess
+
+# Danh sách thư viện cần thiết
+required = ["pygame"]
+
+# Tự động cài nếu chưa có
+for pkg in required:
+    try:
+        __import__(pkg)
+    except ImportError:
+        print(f"Đang cài thư viện {pkg}...")
+        subprocess.check_call([sys.executable, "-m", "pip", "install", pkg])
+
+# Sau khi chắc chắn có pygame mới import
 import random
 import pygame
-import sys
+
 '''
 hướng dẫn chơi :
     0 là ô có thể đào
@@ -9,9 +24,9 @@ hướng dẫn chơi :
 '''
 # game_base hiện thị full map : bom là -1, số bom xung quanh là số khác
 # game_display hiện thị map đã chơi : 0 là chưa chơi, 1 là đã chơi
-n,m,bom=map(int, input('Enter size of map (n row m col numbom): ').split())
-game_base=[[0 for _ in range(m+2)] for _ in range(n+2)]
-game_display=[[9 for _ in range(m+2)] for _ in range(n+2)]
+n, m, bom = map(int, input('Enter size of map (n row m col numbom): ').split())
+game_base = [[0 for _ in range(m+2)] for _ in range(n+2)]
+game_display = [[9 for _ in range(m+2)] for _ in range(n+2)]
 num_bombs = bom
 
 HEIGHT = n*40
@@ -27,17 +42,17 @@ text_lose = pygame.font.Font(None, 72).render("You lose!", True, (255, 255, 255)
 text_lose_rect = text_lose.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 texts = [font.render(str(i), True, (255, 255, 255)) for i in range(0, 10)]
 
-def bomb(n,m,bom,xbase,ybase):
+def bomb(n, m, bom, xbase, ybase):
     while bom:
-        id = random.randint(1,m*n)
-        x=(id-1)//m +1
-        y=(id-1)%m+1
+        id = random.randint(1, m*n)
+        x = (id-1)//m + 1
+        y = (id-1)%m + 1
         if game_base[x][y] != -1 and not(abs(x-xbase) <= 1 and abs(y-ybase) <= 1):
             game_base[x][y] = -1
             bom -= 1
     # đếm số bom xung quanh mỗi ô
-    for i in range(1,n+1):
-        for j in range(1,m+1):
+    for i in range(1, n+1):
+        for j in range(1, m+1):
             if game_base[i][j] != -1:
                 count = 0
                 for x in range(i-1, i+2):
@@ -45,11 +60,11 @@ def bomb(n,m,bom,xbase,ybase):
                         if game_base[x][y] == -1:
                             count += 1
                 game_base[i][j] = count
+
 game_running = 0
 # bắt đầu trò chơi
-step=0
+step = 0
 while True:
-    #print(game_running)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -59,7 +74,7 @@ while True:
             x = pos[1] // 40 + 1
             y = pos[0] // 40 + 1
             if step == 0:
-                bomb(n,m,bom,x,y)
+                bomb(n, m, bom, x, y)
             if game_display[x][y] == 9:
                 game_display[x][y] = game_base[x][y]
                 if game_display[x][y] == -1:
@@ -72,23 +87,21 @@ while True:
     if game_running == 1:
         screen.blit(text_win, text_win_rect)
     elif game_running == 0:
-        for i in range(1,n+1):
-            for j in range(1,m+1):
+        for i in range(1, n+1):
+            for j in range(1, m+1):
                 if(game_display[i][j] == 9):
-                    pygame.draw.rect(screen, ( 0, 200, 0), ((j-1)*40, (i-1)*40, 40, 40), 0)
+                    pygame.draw.rect(screen, (0, 200, 0), ((j-1)*40, (i-1)*40, 40, 40), 0)
                 elif(game_display[i][j] == 0):
                     pygame.draw.rect(screen, (80, 80, 80), ((j-1)*40, (i-1)*40, 40, 40), 0)
                 else:
                     pygame.draw.rect(screen, (80, 80, 80), ((j-1)*40, (i-1)*40, 40, 40), 0)
                     text_rect = texts[game_display[i][j]].get_rect(center=((j-1)*40 + 20, (i-1)*40 + 20))
                     screen.blit(texts[game_display[i][j]], text_rect)
-        for i in range(0,n+1):
+        for i in range(0, n+1):
             pygame.draw.line(screen, (0, 0, 0), (i*40, 0), (i*40, HEIGHT), 1)
-        for j in range(0,m+1):
+        for j in range(0, m+1):
             pygame.draw.line(screen, (0, 0, 0), (0, j*40), (WIDTH, j*40), 1)
     else:
         screen.blit(text_lose, text_lose_rect)
     pygame.display.flip()
     clock.tick(30)
-    
-    
