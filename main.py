a643d03903b08d62a1794a82d3fece785cffc1eb
Khang <khang86175xt@gmail.com>
2026-01-12 22:33:47 +0700

    code lỏ chưa fix

--- main.py
+++ main.py
@@ -16,15 +16,6 @@ for pkg in required:
 import random
 import pygame
 
-'''
-hướng dẫn chơi :
-    0 là ô có thể đào
-    . là ô đã đào và không có bom xung quanh
-    có số khác là ô đã đào và có số bom xung quanh
-'''
-
-# game_base hiện thị full map : bom là -1, số bom xung quanh là số khác
-# game_display hiện thị map đã chơi : 0 là chưa chơi, 1 là đã chơi
 
 n, m, bom = map(int, input('Enter size of map (n row m col numbom): ').split())
 # print('Enter size of map (n row m col numbom): ')
@@ -36,6 +27,9 @@ game_base = [[0 for _ in range(m+2)] for _ in range(n+2)]
 game_display = [[9 for _ in range(m+2)] for _ in range(n+2)]
 num_bombs = bom
 
+# game_base biểu thị full map : bom là -1, các số còn lại là số bom xung quanh
+# game_display hiện thị map đã chơi : 9 là chưa mở, -1 là cờ, các số còn lại là số bom xung quanh
+
 HEIGHT = n*40
 WIDTH = m*40
 pygame.init()
@@ -43,11 +37,14 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Minesweeper")
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
+
 text_win = pygame.font.Font(None, 72).render("You win!", True, (255, 255, 255))
 text_win_rect = text_win.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 text_lose = pygame.font.Font(None, 72).render("You lose!", True, (255, 255, 255))
 text_lose_rect = text_lose.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 texts = [font.render(str(i), True, (255, 255, 255)) for i in range(0, 10)]
+
+flag=pygame.image.load("assets/flag.png").convert_alpha()
 
 def generatebom(n, m, bom, xbase, ybase):
     while bom:
@@ -69,6 +66,10 @@ def generatebom(n, m, bom, xbase, ybase):
                 game_base[i][j] = count
 
 game_running = 0
+#game_running = 0 : đang chơi
+#game_running = 1 : win
+#game_running = -1 : lose
+
 # bắt đầu trò chơi
 step = 0
 while True:
@@ -80,19 +81,36 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and game_running == 0 and event.button == 1:
+        if event.type == pygame.MOUSEBUTTONDOWN and game_running == 0:
             pos = pygame.mouse.get_pos()
             x = pos[1] // 40 + 1
             y = pos[0] // 40 + 1
-            if step == 0:
-                bomb(n, m, bom, x, y)
-            if game_display[x][y] == 9:
-                game_display[x][y] = game_base[x][y]
-                if game_display[x][y] == -1:
+            if event.button == 1:  # left click
+                if step == 0:
+                    generatebom(n, m, bom, x, y)
+                if game_base[x][y] == -1:
                     game_running = -1
-                elif n*m - step - 1 == num_bombs:
-                    game_running = 1
-                step += 1
+                else:
+                    # mở ô
+                    def open_cell(i, j):
+                        global step
+                        if game_display[i][j] != 9:
+                            return
+                        step += 1
+                        game_display[i][j] = game_base[i][j]
+                        if game_base[i][j] == 0:
+                            for xi in range(i-1, i+2):
+                                for yj in range(j-1, j+2):
+                                    if 1 <= xi <= n and 1 <= yj <= m:
+                                        open_cell(xi, yj)
+                    open_cell(x, y)
+            elif event.button == 3:  # right click
+                if game_display[x][y] == 9:
+                    game_display[x][y] = -1
+                elif game_display[x][y] == -1:
+                    game_display[x][y] = 9
+            if n*m - step - 1 == num_bombs:
+                game_running = 1
 
     screen.fill((0, 200, 0))
     if game_running == 1:
@@ -100,19 +118,23 @@ while True:
     elif game_running == 0:
         for i in range(1, n+1):
             for j in range(1, m+1):
-                if(game_display[i][j] == 9):
+                if(game_display[i][j] == 9 or game_display[i][j] == -1):
                     pygame.draw.rect(screen, (30, 190, 30), ((j-1)*40, (i-1)*40, 40, 40), 0)
-                elif(game_display[i][j] == 0):
-                    pygame.draw.rect(screen, (80, 80, 80), ((j-1)*40, (i-1)*40, 40, 40), 0)
+                    if game_display[i][j] == -1:
+                        flag_rect = flag.get_rect(center=((j-1)*40 + 20, (i-1)*40 + 20))
+                        screen.blit(flag,flag_rect)
                 else:
                     pygame.draw.rect(screen, (80, 80, 80), ((j-1)*40, (i-1)*40, 40, 40), 0)
-                    text_rect = texts[game_display[i][j]].get_rect(center=((j-1)*40 + 20, (i-1)*40 + 20))
-                    screen.blit(texts[game_display[i][j]], text_rect)
+                    if(game_display[i][j] > 0):
+                        text_rect = texts[game_display[i][j]].get_rect(center=((j-1)*40 + 20, (i-1)*40 + 20))
+                        screen.blit(texts[game_display[i][j]], text_rect)
         for i in range(0, n+1):
             pygame.draw.line(screen, (0, 0, 0), (i*40, 0), (i*40, HEIGHT), 1)
         for j in range(0, m+1):
             pygame.draw.line(screen, (0, 0, 0), (0, j*40), (WIDTH, j*40), 1)
     else:
         screen.blit(text_lose, text_lose_rect)
+    #text_debug = font.render(f'Steps: {step}', True, (255, 255, 255))
+    #screen.blit(text_debug, (10, 10))
     pygame.display.flip()
     clock.tick(30)
